3382965ee25338381435fa1e18d7f7ff6a7227b6
I339578 <suryam.srivastava@sap.com>
2022-12-26 13:36:59 +0530

    Learning JS

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+console.log(checkSpeed(73));
+
+function checkSpeed(speed) {
+  let points = 0;
+  const checkPoint = 5;
+  const maxSpeed = 70;
+  if (speed >= maxSpeed + checkPoint) {
+    points = (speed - maxSpeed) / checkPoint;
+    if (points >= 12) return "License Suspended";
+    return "Points: " + points;
+  }
+  return "Ok";
+}
